b3c73a0d2215e1ae91766df911515197721b6cd9
lalecsey <lalecsey@gmail.com>
2020-05-05 23:01:24 +0300

    lesson 4 /4

--- lesson_004/04_fractal.py
+++ lesson_004/04_fractal.py
@@ -26,7 +26,24 @@ import simple_draw as sd
 
 # можно поиграть -шрифтами- цветами и углами отклонения
 
-# TODO здесь ваш код
+point_0 = sd.get_point(300, 5)
+
+# def branch(point, angle, length, delta):
+#     if length < 3:
+#         return
+#     v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=1)
+#     v1.draw()
+#     next_point = v1.end_point
+#     next_angle = angle - delta
+#     next_angle_2 = angle + delta
+#     next_length = length * .75
+#     branch(point=next_point, angle=next_angle, length=next_length, delta=delta)
+#     branch(point=next_point, angle=next_angle_2, length=next_length, delta=delta)
+#
+# branch(point=point_0, angle=90, length=100, delta=30)
+
+
+
 
 # 4) Усложненное задание (делать по желанию)
 # - сделать рандомное отклонение угла ветвей в пределах 40% от 30-ти градусов
@@ -35,6 +52,21 @@ import simple_draw as sd
 
 # Пригодятся функции
 # sd.random_number()
+
+
+def branch(point, angle, length, delta):
+    if length < 3:
+        return
+    v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=1)
+    v1.draw()
+    next_point = v1.end_point
+    next_angle = angle - sd.random_number(delta-6, delta+6)
+    next_angle_2 = angle + sd.random_number(delta-6, delta+6)
+    next_length = length * (sd.random_number(70, 80) / 100)
+    branch(point=next_point, angle=next_angle, length=next_length, delta=delta)
+    branch(point=next_point, angle=next_angle_2, length=next_length, delta=delta)
+
+branch(point=point_0, angle=90, length=100, delta=30)
 
 sd.pause()
 
