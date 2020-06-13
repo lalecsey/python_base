877c89eed66cf0e3999cbc5ed809514ef4161d4a
lalecsey <lalecsey@gmail.com>
2020-06-13 21:37:51 +0300

    add lesson 007 / 3

--- lesson_007/03_man_ans_cat.py
+++ lesson_007/03_man_ans_cat.py
@@ -1,14 +1,3 @@
-# -*- coding: utf-8 -*-
-
-from random import randint
-
-# Доработать практическую часть урока lesson_007/python_snippets/08_practice.py
-
-# Необходимо создать класс кота. У кота есть аттрибуты - сытость и дом (в котором он живет).
-# Кот живет с человеком в доме.
-# Для кота дом характеризируется - миской для еды и грязью.
-# Изначально в доме нет еды для кота и нет грязи.
-
 # Доработать класс человека, добавив методы
 #   подобрать кота - у кота появляется дом.
 #   купить коту еды - кошачья еда в доме увеличивается на 50, деньги уменьшаются на 50.
@@ -25,10 +14,195 @@ from random import randint
 
 # Человеку и коту надо вместе прожить 365 дней.
 
-# TODO здесь ваш код
-
 # Усложненное задание (делать по желанию)
 # Создать несколько (2-3) котов и подселить их в дом к человеку.
 # Им всем вместе так же надо прожить 365 дней.
 
 # (Можно определить критическое количество котов, которое может прокормить человек...)
+
+from random import randint
+from termcolor import cprint
+
+class Man:
+
+    def __init__(self, name):
+        self.name = name
+        self.fullness = 50
+        self.house = None
+
+    def __str__(self):
+        return 'Я - {}, сытость {}'.format(
+            self.name, self.fullness)
+
+    def eat(self):
+        if self.house.food >= 10:
+            cprint('{} поел'.format(self.name), color='yellow')
+            self.fullness += 10
+            self.house.food -= 10
+        else:
+            cprint('{} нет еды'.format(self.name), color='red')
+
+    def work(self):
+        cprint('{} сходил на работу'.format(self.name), color='blue')
+        self.house.money += 150
+        self.fullness -= 10
+
+    def watch_MTV(self):
+        cprint('{} смотрел MTV целый день'.format(self.name), color='green')
+        self.fullness -= 10
+
+    def shopping(self):
+        if self.house.money >= 50:
+            cprint('{} сходил в магазин за едой'.format(self.name), color='magenta')
+            self.house.money -= 50
+            self.house.food += 50
+        else:
+            cprint('{} деньги кончились!'.format(self.name), color='red')
+
+    def shopping_for_cats(self):
+        if self.house.money >= 50:
+            cprint('{} сходил в магазин за кошачей едой.'.format(self.name), color='magenta')
+            self.house.money -= 50
+            self.house.food_cat += 50
+        else:
+            cprint('{} деньги кончились!'.format(self.name), color='red')
+
+    def go_to_the_house(self, house):
+        self.house = house
+        self.fullness -= 10
+        cprint('{} Вьехал в дом'.format(self.name), color='cyan')
+
+    def pick_up_cat(self, name_cat):
+        house = self.house
+        cprint('{} подобрал кота {}'.format(self.name, self.name_cat), color='cyan')
+
+
+    def clean_the_house(self):
+        cprint('{} убрался в доме'.format(self.name), color='magenta')
+        self.house.dirt -= 100
+        self.fullness -= 20
+
+    def act(self):
+        if self.fullness <= 0:
+            cprint('{} умер...'.format(self.name), color='red')
+            return
+        dice = randint(1, 6)
+        if self.fullness <= 20:
+            self.eat()
+        elif self.house.food <= 10:
+            self.shopping()
+        elif self.house.food_cat <= 10:
+            self.shopping_for_cats()
+        elif self.house.money <= 50:
+            self.work()
+        elif self.house.dirt > 100:
+            self.clean_the_house()
+        elif dice == 1:
+            self.work()
+        elif dice == 2:
+            self.eat()
+        else:
+            self.watch_MTV()
+
+
+class House:
+
+    def __init__(self):
+        self.food = 50
+        self.money = 50
+        self.food_cat = 0
+        self.dirt = 0
+
+    def __str__(self):
+        return 'В доме еды осталось {}, денег осталось {}, еды у кота {}, грязь в доме {}'.format(
+            self.food, self.money, self.food_cat, self.dirt)
+
+
+class Cat:
+
+    def __init__(self, name):
+        self.name_cat = name
+        self.fullness_cat = 20
+        self.house = None
+
+    def __str__(self):
+        return 'Я - {}, сытость {}'.format(
+            self.name_cat, self.fullness_cat)
+
+    def go_to_the_house(self, house):
+        self.house = house
+        cprint('{} Вьехал в дом'.format(self.name_cat), color='cyan')
+
+    def sleep(self):
+        cprint('{} спит'.format(self.name_cat), color='green')
+        self.fullness_cat -= 10
+
+    def eat(self):
+        if self.house.food_cat >= 10:
+            cprint('{} поел'.format(self.name_cat), color='yellow')
+            self.fullness_cat += 20
+            self.house.food_cat -= 10
+        else:
+            cprint('{} нет еды'.format(self.name_cat), color='red')
+
+    def tear_the_Wallpaper(self):
+        cprint('{} рвёт обои'.format(self.name_cat), color='green')
+        self.fullness_cat -= 10
+        self.house.dirt += 5
+
+    def act(self):
+        if self.fullness_cat <= 0:
+            cprint('{} умер...'.format(self.name_cat), color='red')
+            return
+        dice = randint(1, 6)
+        if self.fullness_cat < 20:
+            self.eat()
+        elif dice == 1:
+            self.sleep()
+        elif dice == 2:
+            self.eat()
+        else:
+            self.tear_the_Wallpaper()
+
+
+
+citizens = [
+   Man(name='Кенни')
+]
+
+cats = [
+    Cat(name='Пушкин')
+]
+
+my_sweet_home = House()
+
+for citisen in citizens:
+    citisen.go_to_the_house(house=my_sweet_home)
+
+for cat in cats:
+    cat.go_to_the_house(house=my_sweet_home)
+
+# for i in range(len(cats))
+#     citizens[0].pick_up_cat(cats[i])
+
+for day in range(1, 366):
+    print('================ день {} =================='.format(day))
+    for citisen in citizens:
+        citisen.act()
+    for cat in cats:
+        cat.act()
+    print('--- в конце дня ---')
+    for cat in cats:
+        print(cat)
+    for citisen in citizens:
+        print(citisen)
+    print(my_sweet_home)
+
+
+# Кот может есть, спать и драть обои - необходимо реализовать соответствующие методы.
+# Когда кот спит - сытость уменьшается на 10
+# Когда кот ест - сытость увеличивается на 20, кошачья еда в доме уменьшается на 10.
+# Когда кот дерет обои - сытость уменьшается на 10, степень грязи в доме увеличивается на 5
+# Если степень сытости < 0, кот умирает.
+# Так же надо реализовать метод "действуй" для кота, в котором он принимает решение
+# что будет делать сегодня
